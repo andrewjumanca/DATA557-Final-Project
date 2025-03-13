cf3209039bd6a71092ab3552cfa23b7d048ed537
jacobmillersd <jacobmillersd@gmail.com>
2025-03-12 18:51:05 -0700

    updated ui for shiny app, see tab 2, added images folder to host images

--- InteractiveDataStory/ui.R
+++ InteractiveDataStory/ui.R
@@ -11,11 +11,44 @@ ui <- fluidPage(
       h3("Does sex bias exist at the university in the most current year available (1995)?"),
       p("Empty.")
     ),
+
+
+
     tabPanel("2",
       h3("Has sex bias existed in the starting salaries of faculty members (salaries in the year
 hired)?"),
-      p("Empty.")
+      
+      fluidRow(
+        column(6, 
+          img(src = "BoxPlotsStartingYearSalary.png", height = "400px"),
+          p("This plot shows the starting salaries of faculty members by gender. The data suggests some differences in salary between male and female faculty in their first year.")
+        ),
+        column(6, 
+          img(src = "HistogramStartingYear.png", height = "400px"),
+          p("This plot compares the starting salaries across different departments, highlighting the variations in salary distributions.")
+        )
+      ),
+      
+      br(),
+      img(src = "Question2MultipleRegression.png", height = "400px"),
+      p('Using multiple regressions allows us to ...'),
+      br(),
+
+      fluidRow(
+        column(6, 
+          img(src = "QQPlot.png", height = "400px"),
+          p("Looking at the QQ Plot for Model 3, as an example, allows us to confirm that our constant variance assumption and normality assumption are met. ")
+        ),
+        column(6, 
+          img(src = "ResidualsVFitted.png", height = "400px"),
+          p("Test")
+        )
+      ),
+
     ),
+
+
+
     tabPanel("3",
       h3("Has sex bias existed in granting salary increases between 1990 -1995?"),
       p("Empty.")
