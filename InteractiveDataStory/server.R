fda59183f834ded640c63f48e371327924d183bb
johntilelli <johntilelli@gmail.com>
2025-03-15 16:49:26 -0700

    Update server.R
    server updates to tab 3 ready for review

--- InteractiveDataStory/server.R
+++ InteractiveDataStory/server.R
@@ -73,6 +73,33 @@ load_and_prepare_data_q3_agg <- function(file_path) {
   return(final_aggregated_data)
 }
 
+load_and_prepare_data_q3_new <- function(file_path){
+  data <- read.table(file_path, header = TRUE)
+  #filter on years 90-95
+  data_filtered_on_year <- data %>%
+    filter(year >= 90 & year <= 95)
+  
+  new_data <- data_filtered_on_year %>%
+    group_by(id) %>%
+    filter(n() > 1) %>%
+    ungroup()
+  
+  # Create a year-centered variable (base year = 1990)
+  new_data$year_c <- new_data$year - 90
+  
+  # Create an experience variable (years since highest degree)
+  new_data$experience <- new_data$year - new_data$yrdeg
+  new_data$experience <- ifelse(new_data$experience < 0, 0, new_data$experience)
+  
+  # Convert categorical variables to factors
+  new_data$sex <- factor(new_data$sex, levels = c("F", "M"))
+  new_data$rank <- factor(new_data$rank, levels = c("Assist", "Assoc", "Full"))
+  new_data$field <- factor(new_data$field, levels = c("Arts", "Prof", "Other"))
+  new_data$deg <- factor(new_data$deg, levels = c("Other", "PhD", "Prof"))
+  #new_data$admin <- as.factor(new_data$admin)
+  return(new_data)
+}
+
 server <- function(input, output) {
   #load aggregated data
   data_agg <- reactive({
@@ -103,6 +130,51 @@ server <- function(input, output) {
       data = data_agg()
     )
     summary(model_percent)
+  })
+  output$regressionConfModelPercent <- renderPrint({
+    model_percent <- lm(
+      percent_increase ~ sex + admin_any + deg + field + highest_rank  + experience + total_years,
+      data = data_agg()
+    )
     confint(model_percent)
+  })
+  
+  new_data <- reactive({
+    load_and_prepare_data_q3_new("salary.txt")
+  })
+  
+  output$plotNewDataLineChart <- renderPlot({
+    summary_stats <- new_data() %>%
+      group_by(year, sex) %>%
+      summarize(avg_salary = mean(salary), .groups = "drop")
+    
+    
+    ggplot(summary_stats, aes(x = year, y = avg_salary, color = sex)) +
+      geom_line(size = 1) +
+      geom_point(size = 2) +
+      labs(title = "Average Salary by Sex (1990–1995)", x = "Year", y = "Average Salary") +
+      theme_minimal()
+    
+  })
+  
+  output$summaryNewModel <- renderPrint({
+    model <- lm(
+      salary ~ year_c * sex + rank + deg + field + admin + experience,
+      data = new_data()
+    )
+    summary(model)
+    
+    #coeftest(model, vcov = vcovHC(model, type = "HC1"))
+    
+    #confint(model)
+    
+    #coefficients <- tidy(model)
+  })
+  output$confNewModel <- renderPrint({
+    model <- lm(
+      salary ~ year_c * sex + rank + deg + field + admin + experience,
+      data = new_data()
+    )
+    confint(model)
   })
 }
