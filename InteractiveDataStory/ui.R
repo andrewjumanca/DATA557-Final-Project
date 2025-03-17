938d95a84867ae9e1c4ee26ed4d44576372ad7ea
jacobmillersd <jacobmillersd@gmail.com>
2025-03-16 19:25:49 -0700

    updated intro to include summary tables and updated tab 2 images to auto size based on monintor size

--- InteractiveDataStory/ui.R
+++ InteractiveDataStory/ui.R
@@ -105,6 +105,14 @@ ui <- fluidPage(
         tags$li("Has sex bias existed in granting promotions from Associate to Full Professor?")
       ),
       br(),
+      p('Below is a brief overview of the data in table form. The first table is information from the entire dataset. The second is filtered by males and third by females to help give a better idea of the information at our disposal.'),
+      img(src = "df_complete_summary.png", style = "display: block; margin: auto; max-width: 100%; height: auto; border: 2px solid black; margin-bottom: 10px;"),
+      p(tags$em('Complete Data Set Summary Statistics'), align= 'center'),
+      img(src = "df_male_summary.png", style = "display: block; margin: auto; max-width: 100%; height: auto; border: 2px solid black; margin-bottom: 10px;"),
+      p(tags$em('Male Filtered Set Summary Statistics'), align= 'center'),
+      img(src = "df_female_summary.png", style = "display: block; margin: auto; max-width: 100%; height: auto; border: 2px solid black; margin-bottom: 10px;"),
+      p(tags$em('Female Filtered Data Set Summary Statistics'), align= 'center'),
+      br(),
       p('Our methods for each question are well documented in each of the following tabs, but as an overview our team used statistical methods such as a two-sample t-test, linear regressions and logistic regressions to answer the questions.
         '),
       br(),
@@ -128,12 +136,12 @@ ui <- fluidPage(
       p('Before diving into advanced statistical methods, we sought to understand the data by using box plots and bar graphs as well as general statistics.'),
       fluidRow(
         column(6,
-          img(src = "BoxPlotsStartingYearSalary.png", height = "300px", style = "border: 2px solid black; margin: 20px;", align = 'center'),
+          img(src = "BoxPlotsStartingYearSalary.png", style = "display: block; margin: auto; max-width: 100%; height: auto; border: 2px solid black; margin-bottom: 10px;"),
           p("This plot shows the starting salaries of faculty members by gender. The box plot shows that the mean for each group is quite similar, though males have 
           far more outliers past the 3rd quartile.")
         ),
         column(6,
-          img(src = "HistogramStartingYear.png", height = "300px", style = "border: 2px solid black; margin: 20px;", align = 'center'),
+          img(src = "HistogramStartingYear.png", style = "display: block; margin: auto; max-width: 100%; height: auto; border: 2px solid black; margin-bottom: 10px;"),
           p("To further understand the underlying data, this bar chart shows the counts associated with multiple salary bands. It is clear again that there are far more outliers in
           the male group. ")
         )
@@ -143,7 +151,7 @@ ui <- fluidPage(
          of males and females. The results of this test showed there was a difference in starting salaries between males and females, which led us to conduct multiple linear regressions.
          The results of the above process can be found below.'),
       br(),
-      img(src = "Question2MultipleRegression.png", height = "475px", style = "border: 2px solid black; margin: 20px;", align = 'center'),
+      img(src = "Question2MultipleRegression.png", style = "display: block; margin: auto; max-width: 100%; height: auto;border: 2px solid black; margin-bottom: 10px;"),
       p('After conducting a two sample t-test and deteriming there was a difference between male and female starting salary, we fit 4 linear regression models to test multiple 
          hypothesis about the factors which were causing the observed difference. We first fit a model with salary as the outcome and sex as the predictor. Then we added factors which
          our team thought may be contributing to the difference. The following regressions are shown in the table as well as their results. Of note, when accounting for variables such as
@@ -153,11 +161,11 @@ ui <- fluidPage(
       p('Our team wanted to make sure that our assumptions were met for our linear models. In the below plots we use model 3 to check for normality, linearity, and constant variance.'),
       fluidRow(
         column(6, 
-          img(src = "QQPlot.png", height = "300px", style = "border: 2px solid black; margin: 20px;", align = 'center'),
+          img(src = "QQPlot.png", style = "display: block; margin: auto; max-width: 100%; height: auto; border: 2px solid black; margin-bottom: 10px;"),
           p("Looking at the QQ Plot for Model 3, as an example, allows us to confirm that our normality assumption is met. ")
         ),
         column(6, 
-          img(src = "ResidualsVFitted.png", height = "300px", style = "border: 2px solid black; margin: 20px;", align = 'center'),
+          img(src = "ResidualsVFitted.png", style = "display: block; margin: auto; max-width: 100%; height: auto; border: 2px solid black; margin-bottom: 10px;"),
           p("Looking at the Residuals vs Fitted Values plot for Model 3, allows us to confirm that our linearity and constant variance assumptions are met."),
         )
       ),
